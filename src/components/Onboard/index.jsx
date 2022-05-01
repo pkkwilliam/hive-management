6a6ab6727aa7fe9ca06f3c1e12557becb50ebc29
pkkwilliam <pkkwilliam@me.com>
2022-05-01 17:33:19 +0800

    update onboard

--- src/components/Onboard/index.jsx
+++ src/components/Onboard/index.jsx
@@ -14,8 +14,6 @@ import Title from 'antd/lib/typography/Title';
 export const Onboard = () => {
   const [companyOnboard, setCompanyOnboard] = useState({});
 
-  let showOnboard = true;
-
   useEffect(() => {
     getCompanyOnboard();
   }, []);
@@ -23,35 +21,22 @@ export const Onboard = () => {
   const getCompanyOnboard = async () => {
     const response = await GET_COMPANY_ONBOARD();
     setCompanyOnboard(response);
+  };
 
-    // showOnboard
+  const showOnboard = () => {
     let showOnboard = false;
-    for (let key in response) {
-      if (response[key]) {
+    for (let key in companyOnboard) {
+      if (companyOnboard[key]) {
         showOnboard = true;
         break;
       }
     }
+    return showOnboard;
   };
 
   return (
     <>
-      <Title level={4}>使用嚮導</Title>
-      <Alert message="使用嚮導助您快速熟悉系統及添加必需要的組件" type="success" showIcon banner />
-      <Space direction="vertical" style={{ display: 'flex' }}>
-        <ShopOnboard />
-        <CategoryOnboard />
-        <ItemOnboard />
-        <CompanyBusinessOnboard />
-        <OrderOnobard />
-        <PriceTemplateOnboard />
-      </Space>
-    </>
-  );
-
-  return (
-    <>
-      {showOnboard ? (
+      {showOnboard() ? (
         <>
           <Title level={4}>使用嚮導</Title>
           <Alert
@@ -65,6 +50,7 @@ export const Onboard = () => {
             {companyOnboard.showCreateCategory ? <CategoryOnboard /> : null}
             {companyOnboard.showCreateItem ? <ItemOnboard /> : null}
             {companyOnboard.showCreateCompanyBusiness ? <CompanyBusinessOnboard /> : null}
+            {companyOnboard.showCreateOrder ? <OrderOnobard /> : null}
             {companyOnboard.showCreatePriceTemplate ? <PriceTemplateOnboard /> : null}
           </Space>
         </>
